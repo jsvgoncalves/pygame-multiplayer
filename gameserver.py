302adcd39d6eef921b89e0d10a58159b67b4689d
João Gonçalves <jsvgoncalves@gmail.com>
2015-02-21 13:44:52 +0100

    server: major refactoring
    * Extracts player's responsability function to Player class.
    * Extracts main methods for communication and parsing.
    
    Signed-off-by: Joao Goncalves <jsvgoncalves@gmail.com>

--- gameserver.py
+++ gameserver.py
@@ -20,10 +20,24 @@ class Player():
     def __init__(self, name="Bot"):
         self.name = name
         self.position = (0, 0)
+        self.stepsize = 5
         pass
 
     def get_name(self):
         return self.name
+
+    def do_movement(self, mv):
+        pos = self.position
+        if mv == "u":
+            pos = (pos[0], max(0, pos[1] - self.stepsize))
+        elif mv == "d":
+            pos = (pos[0], min(300, pos[1] + self.stepsize))
+        elif mv == "l":
+            pos = (max(0, pos[0] - self.stepsize), pos[1])
+        elif mv == "r":
+            pos = (min(400, pos[0] + self.stepsize), pos[1])
+
+        self.position = pos
 
 
 class GameServer(object):
@@ -35,58 +49,61 @@ class GameServer(object):
         self.read_list = [self.listener]
         self.write_list = []
 
-        self.stepsize = 5
         self.players = {}
 
-    def do_movement(self, mv, player):
-        pos = self.players[player].position
-        if mv == "u":
-            pos = (pos[0], max(0, pos[1] - self.stepsize))
-        elif mv == "d":
-            pos = (pos[0], min(300, pos[1] + self.stepsize))
-        elif mv == "l":
-            pos = (max(0, pos[0] - self.stepsize), pos[1])
-        elif mv == "r":
-            pos = (min(400, pos[0] + self.stepsize), pos[1])
-
-        self.players[player].position = pos
-
     def run(self):
-        print "Waiting..."
+        """Main server loop
+        """
+        print "Server ready."
         try:
             while True:
+                # blocking select
                 readable, writable, exceptional = select.select(
                     self.read_list,
                     self.write_list,
                     [])
 
-                for f in readable:
-                    if f is self.listener:
-                        msg, addr = f.recvfrom(32)
-                        if len(msg) >= 1:
-                            cmd = msg[0]
-                            if cmd == "c":  # New Connection
-                                self.players[addr] = Player(addr)
-                            elif cmd == "u":  # Movement Update
-                                if len(msg) >= 2 and addr in self.players:
-                                    # Second char of message is
-                                    # direction (udlr)
-                                    self.do_movement(msg[1], addr)
-                            elif cmd == "d":  # Player Quitting
-                                if addr in self.players:
-                                    del self.players[addr]
-                            else:
-                                print "Unexpected: {0}".format(msg)
-                for addr, player in self.players.iteritems():
-                    send = []
-                    for addr2, playerpos in self.players.iteritems():
-                        pos = (repr(playerpos.position[0]) + "," +
-                               repr(playerpos.position[1]))
-                        send.append(pos)
-                    self.listener.sendto('|'.join(send), addr)
+                # Parses incoming message
+                self.parse_message(readable)
+                # Notifies players of changes
+                self.update_players()
+
         except KeyboardInterrupt as e:
             print(e)
             pass
+
+    def parse_message(self, readable):
+        """Parses incoming message from socket
+        """
+        for f in readable:
+            if f is self.listener:
+                msg, addr = f.recvfrom(32)
+                if len(msg) >= 1:
+                    cmd = msg[0]
+                    if cmd == "c":  # New Connection
+                        self.players[addr] = Player(addr)
+                    elif cmd == "u":  # Movement Update
+                        if len(msg) >= 2 and addr in self.players:
+                            # Second char of message is
+                            # direction (udlr)
+                            self.players[addr].do_movement(
+                                msg[1])
+                    elif cmd == "d":  # Player Quitting
+                        if addr in self.players:
+                            del self.players[addr]
+                    else:
+                        print "Unexpected: {0}".format(msg)
+
+    def update_players(self):
+        """Sends all the players the new game state
+        """
+        for addr, player in self.players.iteritems():
+            send = []
+            for addr2, playerpos in self.players.iteritems():
+                pos = (repr(playerpos.position[0]) + "," +
+                       repr(playerpos.position[1]))
+                send.append(pos)
+            self.listener.sendto('|'.join(send), addr)
 
 if __name__ == "__main__":
     g = GameServer()
